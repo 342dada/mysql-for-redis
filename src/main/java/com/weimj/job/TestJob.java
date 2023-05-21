cacf2a748a5ac82a17840e18cf15d651f1acccf2
weimengjie <932141003@qq.com>
2023-05-21 23:43:06 +0800

    test

--- src/main/java/com/weimj/job/TestJob.java
+++ src/main/java/com/weimj/job/TestJob.java
@@ -2,10 +2,6 @@ package com.weimj.job;
 
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
-import com.weimj.core.Process;
-import com.weimj.entity.ProcessParam;
-import com.weimj.mapper.TaskMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class TestJob implements SimpleJob {
 
